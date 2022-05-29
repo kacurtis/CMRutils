65592ec2817703f95fd3f67916defff3baee2e97
Alex Curtis <37004191+kacurtis@users.noreply.github.com>
2022-05-29 15:54:33 -0700

    packaged and documented

--- R/import_effort.r
+++ R/import_effort.r
@@ -1,23 +1,21 @@
-# import data from CRC Sighting databases
-
+#' @title Import effort data
+#' 
+#' @description 
+#' Import data from CRC Sighting databases
+#' 
+#' @details 
+#' Assumes effort data files ('CRC Sighting Data YYYY.mdb') all stored in one directory
+#' 
+#' @param datdir Directory path for effort data files
+#' @param effyrs Years for which to import effort
+#' 
+#' @return list of data frames for with effort, effort events, and event codes
+#' 
+#' @export
 import_effort <- function(datdir, effyrs = NULL) {
 
-  # import CRC Sightings databases
-  # Note: function assumes effort data files ('CRC Sighting Data YYYY.mdb') all stored in one directory
-  # 
-  # Arguments
-  # datdir    directory path for data files
-  # effyrs    years of interest for effort
-  # 
-  # Returns
-  # list with effort, effort events, and event code tables
-
-  # load library
-  library(DBI)
-  library(dplyr)
-  
   if (is.null(effyrs)) 
-    stop("Must specify at least one year of interest for effort")
+    stop("Must specify at least one year for which to import effort")
     
   eff <- data.frame(NULL)
   effev <- data.frame(NULL)
@@ -28,21 +26,19 @@ import_effort <- function(datdir, effyrs = NULL) {
     driver_string <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
     dbq_string <- paste0("DBQ=",datdir,"CRC Sighting Data ",y,".mdb")
     db_connect_string <- paste0(driver_string, dbq_string)
-    myconn <- dbConnect(odbc::odbc(), .connection_string = db_connect_string)
+    myconn <- DBI::dbConnect(odbc::odbc(), .connection_string = db_connect_string)
     # fetch data
-    tempeff <- dbReadTable(myconn, "tDailyEffort")
-    tempev <- dbReadTable(myconn, "tDailyEvents")
-    tempevcode <- dbReadTable(myconn, "ltEventCodes") %>%   # accepts variation in capitalization of table names in some years
-      mutate(yy = y)
+    tempeff <- DBI::dbReadTable(myconn, "tDailyEffort")
+    tempev <- DBI::dbReadTable(myconn, "tDailyEvents")
+    tempevcode <- DBI::dbReadTable(myconn, "ltEventCodes") %>%   # accepts variation in capitalization of table names in some years
+      dplyr::mutate(yy = y)
     # disconnect
-    dbDisconnect(myconn)
+    DBI::dbDisconnect(myconn)
     # add to data frames
-    eff <- bind_rows(eff, tempeff)
-    effev <- bind_rows(effev, tempev)
-    evcode <- bind_rows(evcode, tempevcode)
+    eff <- dplyr::bind_rows(eff, tempeff)
+    effev <- dplyr::bind_rows(effev, tempev)
+    evcode <- dplyr::bind_rows(evcode, tempevcode)
   }
     
-  detach("package:DBI")
-  
   return(list(eff=eff, effev=effev, evcode=evcode))
 }
--- R/import_mnid.r
+++ R/import_mnid.r
@@ -1,32 +1,29 @@
-# import data from MN ID database
-
+#' @title Import MN ID data
+#' 
+#' @description 
+#' Import data from MN ID database, including humpback sightings, location code 
+#' reference table, and biopsy sample table
+#' 
+#' @param datdir Directory path for MN ID database file
+#' @param mnidfile File path for MN ID database file
+#' 
+#' @return list with sightings, loccode, and biopsy sample tables
+#' 
+#' @export
 import_mnid <- function(datdir, mnidfile) {
 
-  # import MN ID database
-  # 
-  # Arguments
-  # datdir    directory path for data files
-  # mnidfile  file path for MN ID database file
-  # 
-  # Returns
-  # list with sightings, loccode, and biopsy tables
-
-  # load library
-  library(DBI)
-  
   # get connection to MN ID db
   driver_string <- "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
   dbq_string <- paste0("DBQ=",datdir,mnidfile)
   db_connect_string <- paste0(driver_string, dbq_string)
-  myconn <- dbConnect(odbc::odbc(), .connection_string = db_connect_string)
+  myconn <- DBI::dbConnect(odbc::odbc(), .connection_string = db_connect_string)
   # fetch data
-  sight <- dbReadTable(myconn, "MN ID Sightings")
-  loccode <- dbReadTable(myconn, "Loc Code")
-  biop <- dbReadTable(myconn, "Mn ID Samples")
+  sight <- DBI::dbReadTable(myconn, "MN ID Sightings")
+  loccode <- DBI::dbReadTable(myconn, "Loc Code")
+  biop <- DBI::dbReadTable(myconn, "Mn ID Samples")
   # disconnect
-  dbDisconnect(myconn)
+  DBI::dbDisconnect(myconn)
   rm(driver_string, dbq_string, db_connect_string, myconn)
-  detach("package:DBI")
-  
+
   return(list(sight=sight, loccode=loccode, biop=biop))
 }
